0f5d6e101cf4132575b6118238ef6ead9f813f02
TheLettuc3 <123341702+TheLettuc3@users.noreply.github.com>
2024-01-22 21:19:56 -0600

    Added base code to IntakeCommand.

--- src/main/java/frc/robot/Commands/IntakeCommands/IntakeCommand.java
+++ src/main/java/frc/robot/Commands/IntakeCommands/IntakeCommand.java
@@ -1,7 +1,40 @@
 package frc.robot.Commands.IntakeCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
 
 public class IntakeCommand extends Command {
+    // Declare subsystem variables
+    private final IntakeSubsystem kIntakeSubsystem;
 
+    // Declare subsystem state (i.e. status) and initialize
+    private boolean goodHealth = true;
+
+    public IntakeCommand(IntakeSubsystem intake) {
+        addRequirements(intake);
+        kIntakeSubsystem = intake;
+    }
+
+    @Override
+    public void initialize() {
+        //Initializes intake
+    }
+
+    @Override
+    public void execute() {
+        //Continue intaking
+        if (goodHealth) {
+            kIntakeSubsystem.takeIn();
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        //End intake
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
 }

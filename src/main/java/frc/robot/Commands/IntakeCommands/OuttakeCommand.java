7db1894703cb06032d5df0be029cc15cb46afcb7
TheLettuc3 <123341702+TheLettuc3@users.noreply.github.com>
2024-01-23 19:42:56 -0600

    Fixed spacing.

--- src/main/java/frc/robot/Commands/IntakeCommands/OuttakeCommand.java
+++ src/main/java/frc/robot/Commands/IntakeCommands/OuttakeCommand.java
@@ -22,8 +22,8 @@ public class OuttakeCommand extends Command {
     @Override
     public void execute() {
         //Continue outtaking
-            kOuttakeSubsystem.takeOut();
-        }
+        kOuttakeSubsystem.takeOut();
+    }
 
 
     @Override

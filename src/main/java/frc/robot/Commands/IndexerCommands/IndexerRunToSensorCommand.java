2e8cc1ce6d0890d17bdf49a94328f1bbcae709af
Robbie Allen <raallen3.14@gmail.com>
2024-01-23 19:12:02 -0600

    Added code to execute

--- src/main/java/frc/robot/Commands/IndexerCommands/IndexerRunToSensorCommand.java
+++ src/main/java/frc/robot/Commands/IndexerCommands/IndexerRunToSensorCommand.java
@@ -1,7 +1,34 @@
 package frc.robot.Commands.IndexerCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IndexerSubsystem;
 
 public class IndexerRunToSensorCommand extends Command {
+    private final IndexerSubsystem m_indexer;
 
+    public IndexerRunToSensorCommand(IndexerSubsystem indexer) {
+        addRequirements(indexer);
+        m_indexer = indexer;
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        m_indexer.intake();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        // Have we reached our destination?
+        return true;
+    }
 }

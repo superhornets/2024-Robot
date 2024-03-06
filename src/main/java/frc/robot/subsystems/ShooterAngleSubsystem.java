b1dcc0c42082a9e93e70288e35753f9188619168
Felarof1 <98996513+Felarof1@users.noreply.github.com>
2024-03-05 18:32:43 -0600

    Revert "added code for shooter angle"
    This reverts commit 99109e098425491376ff7177d430262d9707269e.

--- src/main/java/frc/robot/subsystems/ShooterAngleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterAngleSubsystem.java
@@ -7,8 +7,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkLimitSwitch;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.CANSparkBase.ControlType;
-
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -20,13 +18,6 @@ public class ShooterAngleSubsystem extends SubsystemBase {
     private final CANSparkMax m_motor = new CANSparkMax(ShooterAngleConstants.kMotorCanId, MotorType.kBrushless);
     private final SparkPIDController m_pidController = m_motor.getPIDController();
     private final AbsoluteEncoder m_encoder = m_motor.getAbsoluteEncoder(Type.kDutyCycle);
-
-    private final TrapezoidProfile m_profile = new TrapezoidProfile(
-            new TrapezoidProfile.Constraints(ShooterAngleConstants.kMaxVelocity, ShooterAngleConstants.kMaxAccel));
-    private TrapezoidProfile.State m_goal = new TrapezoidProfile.State();
-    private TrapezoidProfile.State m_setpoint = new TrapezoidProfile.State();
-
-    private static double kDt = 0.02;
 
     public ShooterAngleSubsystem() {
         // Initialize anything else that couldn't be initialized yet
@@ -47,15 +38,14 @@ public class ShooterAngleSubsystem extends SubsystemBase {
         m_pidController.setPositionPIDWrappingMinInput(ShooterAngleConstants.kAbsoluteEncoderPositionPIDMinInput);
         m_pidController.setPositionPIDWrappingMaxInput(ShooterAngleConstants.kAbsoluteEncoderPositionPIDMaxInput);
 
-        //m_pidController.setSmartMotionMinOutputVelocity(ShooterAngleConstants.kMinVelocity, 0);
-        //m_pidController.setSmartMotionMaxVelocity(ShooterAngleConstants.kMaxVelocity, 0);
-        //m_pidController.setSmartMotionMaxAccel(ShooterAngleConstants.kMaxAccel, 0);
+        m_pidController.setSmartMotionMinOutputVelocity(ShooterAngleConstants.kMinVelocity, 0);
+        m_pidController.setSmartMotionMaxVelocity(ShooterAngleConstants.kMaxVelocity, 0);
+        m_pidController.setSmartMotionMaxAccel(ShooterAngleConstants.kMaxAccel, 0);
         m_pidController.setOutputRange(ShooterAngleConstants.kMinOutput, ShooterAngleConstants.kMaxOutput);
 
         this.setDefaultCommand(new RunCommand(() -> {
             holdPosition();
         }, this));
-
     }
 
     public double getPosition() {
@@ -67,11 +57,8 @@ public class ShooterAngleSubsystem extends SubsystemBase {
     }
 
     public void moveTo(double angle) {
-        //m_pidController.setReference(angle, ControlType.kPosition);
-
-        m_goal = new TrapezoidProfile.State(angle, 0);
-        m_setpoint = m_profile.calculate(kDt, m_setpoint, m_goal);
-        m_pidController.setReference(m_setpoint.position, ControlType.kPosition);
+        m_pidController.setReference(angle, ControlType.kPosition);
+        //m_motor.set(0);
     }
 
     public void moveUp() {
